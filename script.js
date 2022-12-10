c36055f4f63c04e134d506a1667e61b92fb66648
Babi <108243062+bpsoraggi@users.noreply.github.com>
2022-12-10 16:23:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ $(document).ready(function () {
     });
 
     var typed = new Typed(".typing", {
-        strings: ["Aluna", "Programadora", "Designer", "Freelancer"],
+        strings: ["Student", "Programmer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Aluna", "Programadora", "Designer", "Freelancer"],
+        strings: ["Student", "Programmer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
